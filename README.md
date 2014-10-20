fc17c2f34f3eb301a9c0faf46e6d54ca011376a3
shameerc <shameer806@gmail.com>
2014-10-20 22:03:17 +0400

    added some code comments

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-##Conway's game of life
+##[Conway's game of life
 
-This is a simple JavaScript implementation of Conway's game of life.It will never be complete as I'm using this repository for learning and experimenting. I'm glad if you can find something useful here :)
+This is a simple JavaScript implementation of Conway's game of life. It will never be complete as I'm using this repository for learning and experimenting. I'm glad if you can find something useful here :)
 
 ####TODO
 * Make infinite grid.
-* Create a pattern selector instead of hard coding different patterns.
+* Create a pattern selector interface instead of hard coding different patterns.
---
+++ game.js
@@ -0,0 +1,190 @@
+/**
+* A Cell
+*/
+function Cell(x, y, cellSize, life) {
+  this.x = x ;
+  this.y = y ;
+  this.radius = cellSize / 2;
+  this.cellSize = cellSize;
+  this.life = life;
+
+  /**
+  * Draw the cell in canvas
+  * @param Object ctx  canvas context
+  */
+  this.draw = function(ctx){
+    ctx.fillStyle = 'orange';
+    ctx.beginPath();
+    ctx.arc(( 
+        this.x * this.cellSize) + this.radius
+      , (this.y * this.cellSize) + this.radius 
+      , this.radius
+      , 0
+      , 2 * Math.PI
+      , false
+    );
+    ctx.fill(); 
+  }
+}
+    
+function Game(canvas) {
+  this.canvas = document.getElementById(canvas);
+  this.ctx = this.canvas.getContext('2d');
+  this.current = []; 
+  this.CELL_SIZE = 10;
+  this.ALIVE = 1;
+  this.DEAD = 0;
+  this.TICK = 500;
+  this.running = false;
+  var that = this;
+  var interval = null;
+
+  /** 
+  * Initialize the game. Prepare the initial grid
+  * @param array initalState seed/initial pattern
+  */
+  this.init = function(initalState) {
+    // Create the empty grid
+    for(var i = 0; i < this.canvas.width/ this.CELL_SIZE; i++) {
+      this.current[i] = [];
+      for(var j = 0; j <this.canvas.height/this.CELL_SIZE; j++) {
+        this.current[i].push(new Cell(i, j, this.CELL_SIZE, this.DEAD));
+      }
+    }
+
+    // Mark live cells based on intial state
+    initalState.forEach(function(coords){
+      if(that.current[coords[0]] &&
+        that.current[[coords[0]]][[coords[1]]]) {
+          that.current[[coords[0]]][[coords[1]]].life = that.ALIVE;
+      }
+    })
+  }
+
+  /**
+  * Draw live cells in the canvas
+  */
+  this.drawCells = function() {
+    this.current.forEach(function(row){
+      row.forEach(function(cell) {
+        // Check if the cell is alive
+        if(cell.life == that.ALIVE){
+          // Draw cell on canvas
+          cell.draw(that.ctx);
+        }
+      })
+    });
+  }
+
+  /**
+  * Find the next generation of cells
+  * based on the rules of game of life
+  */
+  this.findNextStep = function() {
+    var next = this.current.map(function(row){
+      return row.map(function(cell){
+        // Calculate the neighbours of cell
+        var neighbours = that.countNeighbours(cell);
+        var life = ( (neighbours == 3 ) // If number of neighbours is exactly three
+                                        // Or if live cell has 2 neighbours
+                        || ( neighbours == 2 && cell.life == that.ALIVE ))
+                    ? that.ALIVE  // The cell will be live
+                    : that.DEAD;  // Or it is a dead cell
+        return new Cell(cell.x, cell.y, that.CELL_SIZE, life)
+      });
+    });
+
+    // Set the next generation as the current generation
+    this.current = next;
+  }
+
+  /**
+  * Count live neighbours of a cell.
+  * @param Object cell
+  */
+  this.countNeighbours = function(cell) {
+    return [-1,0,1].map(function(row){
+      return [-1,0,1].reduce(function(val, j){
+        return ( 
+          (row == 0 && j==0)
+          || !that.current[cell.x + row]
+          || !that.current[cell.x + row][cell.y + j] )
+            ? val
+            : val + that.current[cell.x + row][cell.y + j].life
+      }, 0)
+    }).reduce(function(sum, i){
+      return sum+i;
+    },0);
+  }
+
+  /**
+  * Clear canvas
+  */
+  this.clearCanvas = function() {
+    this.ctx.clearRect(0,0, this.canvas.height, this.canvas.height);
+  }
+
+  /*
+  * The game loop
+  * Find the next generation
+  * Clar the canvas
+  * Draw the cells
+  */
+  this.gameLoop = function() {
+    that.findNextStep();
+    that.clearCanvas();
+    that.drawCells();
+  }
+
+  /**
+  * Start the game
+  * @param array initial state
+  */
+  this.start = function(initalState) {
+    this.stop();
+    this.running = true;
+    this.init(initalState);
+    that.clearCanvas();
+    that.drawCells();
+    interval = setInterval(this.gameLoop, that.TICK);
+  }
+
+  /**
+  * Resume the game if it is not running
+  */
+  this.continue = function() {
+    if(this.running == false) {
+      this.running = true;
+      interval = setInterval(this.gameLoop, that.TICK);
+    }
+  }
+
+  /**
+  * Stop the game if it is running
+  */
+  this.stop = function() {
+    if(this.running == true) {
+      this.running = false;
+      clearInterval(interval);
+    }
+  }
+
+}
+
+var types = {
+  glider : [[10,10],[11,11],[12,11],[10,12],[11,12]],
+  oscillator: [[10,10],[10,11],[10,12]],
+  boat:[[10,10],[9,11],[11,11],[10,12],[11,12]],
+  something:[[10,10],[10,11],[10,12],[11,11],[12,11]]
+}
+
+var game = new Game("canvas");
+game.start(types['something']);
+
+document.getElementById('life_type').onchange = function() {
+  var type = this.value;
+  if(type !="") {
+    game.stop();
+    game.start(types[type]);
+  }
+}
